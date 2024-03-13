7f15a1abf7a6b557e43c619c58f396e919f1ae1a
Kamni-123 <singhkamni2002@gmail.com>
2024-03-13 15:30:53 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { ColorModeContext, useMode } from "./Theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import "./App.css";
+import TopBoard from "./scenes/global/TopBoard";
+import Dashboard from "./scenes/dashboard/dashboard";
+import SideBar from "./scenes/global/SideBar";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const [theme, colorMode] = useMode(ThemeProvider);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div style={{ display: 'flex' }}>
+  <SideBar />
+  <div style={{ flex: 1 }}>
+    <TopBoard />
+    <main>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </main>
+  </div>
+</div>
+
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
